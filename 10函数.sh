2c0e3b78a466d40a5c3ba89fff3300419abb6c23
HuangCongQing <1756260160@qq.com>
2020-06-15 15:38:25 +0000

    函数

---
+++ 10函数.sh
@@ -0,0 +1,24 @@
+# 函数
+
+
+# 1、可以带function fun() 定义，也可以直接fun() 定义,不带任何参数。
+# 2、参数返回，可以显示加：return 返回，如果不加，将以最后一条命令运行结果，作为返回值。 return后跟数值n(0-255
+
+demoFun(){
+    echo "sdfvbn"
+}
+demoFun  # 么有()
+
+
+
+funWithReturn(){
+    echo "这个函数会对输入的两个数字进行相加运算..."
+    echo "输入第一个数字: "
+    read aNum
+    echo "输入第二个数字: "
+    read anotherNum
+    echo "两个数字分别为 $aNum 和 $anotherNum !"
+    return $(($aNum+$anotherNum))
+}
+funWithReturn
+echo "输入的两个数字之和为 $? !"
