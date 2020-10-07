08c15268168afffb32f4627680533039c90ac6db
HuangCongQing <1756260160@qq.com>
2020-10-07 18:03:55 +0800

    update: 修改权限777

---
+++ example1.sh
@@ -0,0 +1,10 @@
+###
+ # @Description: 
+ # @Author: HCQ
+ # @Company(School): UCAS
+ # @Date: 2020-10-07 17:40:40
+ # @LastEditors: HCQ
+ # @LastEditTime: 2020-10-07 17:48:23
+### 
+# Shell编程实战丨老男孩IT教育-理解shell精髓  https://www.bilibili.com/video/BV1vE41127wm?p=3 
+echo "例子1："
