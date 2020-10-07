c30a507a1a446ee490a9b9a272e5f75ac9b69c19
HuangCongQing <1756260160@qq.com>
2020-10-07 19:28:18 +0800

    add: 流程

--- example1.sh
+++ example1.sh
@@ -5,7 +5,7 @@
  # @Company(School): UCAS
  # @Date: 2020-10-07 17:40:40
  # @LastEditors: HCQ
- # @LastEditTime: 2020-10-07 18:53:20
+ # @LastEditTime: 2020-10-07 19:26:49
 ### 
 echo "例子1："
 
@@ -14,4 +14,10 @@ echo "两数和：$var"
 
 # var=`expr 2 + 2 `
 # echo "两数之和：$var"
-
+a=10;b=3
+if [ $a != $b ]
+then
+echo "不相等"
+else 
+echo "相等"
+fi
