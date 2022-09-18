1b8a666f3faf66605bc8b6d16c99ce3c544196a4
hcq <1756260160@qq.com>
2022-09-18 14:40:52 +0800

    add: 方法2：dialog提示窗口

---
+++ task/02shell弹窗提示小工具/02dialog提示窗口.sh
@@ -0,0 +1,11 @@
+
+###
+ # @Description: 方法2：dialog提示窗口  https://www.yuque.com/huangzhongqing/lang/bggvfu
+ # @Author: HCQ
+ # @Company(School): UCAS
+ # @Email: 1756260160@qq.com
+ # @Date: 2022-09-18 12:54:10
+ # @LastEditTime: 2022-09-18 12:58:24
+ # @FilePath: /shell/task/02shell弹窗提示小工具/02dialog提示窗口.sh
+### 
+dialog --msgbox text 20 100
